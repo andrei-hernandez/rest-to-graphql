c8a17948575823fbc4b59b37dc9a571125991d89
AndiUchiha <andrei@devany.mx>
2021-04-24 20:11:59 +0000

    initial commit

---
+++ src/resolvers/query.ts
@@ -0,0 +1,9 @@
+import { IResolvers } from "graphql-tools";
+
+// Los resolvers de las operaciones de consulta para devolver información
+const resolvers: IResolvers = {
+    Query: {
+    }
+};
+
+export default resolvers;
---
+++ src/resolvers/type.ts
@@ -0,0 +1,9 @@
+import { IResolvers } from "graphql-tools";
+import { getWikipediaMobileUrl } from "../lib/utils";
+
+// Los resolvers de las operaciones de consulta para devolver información
+const type: IResolvers = {
+
+};
+
+export default type;
